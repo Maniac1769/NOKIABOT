eb1c5c17a23d1e3c0711ae772826b31c2fa0d69e
Abhinav Mathur <91373992+Maniac1769@users.noreply.github.com>
2024-04-17 15:19:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from langchain.document_loaders.csv_loader import CSVLoader
 from utils import get_model_response
